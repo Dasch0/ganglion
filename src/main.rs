d92af5be405e74d521db8094ac9d915b05f4119a
David Schwarz <dsdavidschwarz@gmail.com>
2020-08-09 09:35:02 -0500

    complete nnfs.io ch1 and ch2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
-use ferris_says;
-use std;
+
+// ND array imported as default since linalg is core to the project
+// All other modules follow the practice of explicit namespaces
+use ndarray::*;
 
 fn main() {
-  let stdout = std::io::stdout();
-  let message = String::from("Hello, I'm a crab!");
-  let width = message.chars().count();
+  let inputs = arr2(&[[1.0, 2.0, 3.0, 2.5],
+                     [2.0, 5.0, -1.0, 2.0],
+                     [-1.5, 2.7, 3.3, -0.8]]);
+  println!("inputs:\n{:#.2}\n", inputs);
 
-  let mut writer = std::io::BufWriter::new(stdout.lock());
-  ferris_says::say(message.as_bytes(), width, &mut writer).unwrap();
+  let weights = arr2(&[[0.2, 0.8, -0.5, 1.0],
+                      [0.5, -0.91, 0.26, -0.5],
+                      [-0.26, -0.27, 0.17, 0.87]]);
+  println!("weights:\n{:#.2}\n", weights);
+  
+  let biases = arr1(&[2.0, 3.0, 0.5]);
+  println!("biases:\n{:#.2}\n", biases);
+
+  let layer_outputs = inputs.dot(&weights.reversed_axes()) + biases;
+
+  println!("layer outputs:\n{:#.2}\n", layer_outputs); 
 }
-
